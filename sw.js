72912dae64ec8523f272fe5461c0411b84fd6e7b
Saidislom Saidazimov <saidazimovsaidislom97@gmail.com>
2026-09-26 19:30:25 +0500

    Fix game HUD header alignment on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'candy-blast-v38';
+const CACHE_NAME = 'candy-blast-v39';
 // The launch shell stays small; large 3D and journey artwork is cached when used.
 const CORE = [
   '/js/profiles.js',
